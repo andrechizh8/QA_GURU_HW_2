5278d9fba15d9ab32520cbd202f652adf43f8b91
andrechizh8 <andrechizh.ru@yandex.ru>
2022-11-25 14:52:09 +0300

    first commit in new_branch

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-def test_1():
-    assert 1 == 1
+def test_01():
+    assert 10 == 10
 
-def test_2():
-    assert 2 == 2
+def test_02():
+    assert 20 == 20
 
-def test_3():
-    assert 3 == 3
+def test_03():
+    assert 30 == 30
 
